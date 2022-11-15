3b1df35485ad01f3fd0256cae7af1e212410c14b
AlessandroVola <volaalessandro@gmail.com>
2022-11-15 17:14:50 +0100

    fillable for Post Model

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    //titolo
+    //contenuto
+    //slug
+
+    protected $fillable =['title', 'content', 'slug'];
 }
+ 
