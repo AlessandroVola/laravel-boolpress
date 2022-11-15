d6e312af4cef5c669c2ddfd08a14ad6fe9d8f7b6
AlessandroVola <volaalessandro@gmail.com>
2022-11-15 18:33:35 +0100

    added edit and create Links

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,4 +3,8 @@
 @section('content')
     <h2>{{ $post->title }}</h2>
     <p>{{ $post->content }}</p>
+
+    <div>
+        <a href="{{ route('admin.posts.edit', $post->id) }}">Edit Post</a>
+    </div>
 @endsection
