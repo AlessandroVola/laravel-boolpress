f12b233750554c8404a4ac2014f8c673c5617d3e
AlessandroVola <volaalessandro@gmail.com>
2022-11-17 23:22:48 +0100

    implemented no category in old post

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,6 +4,13 @@
     <h2>{{ $post->title }}</h2>
     <p>{{ $post->content }}</p>
 
+    {{-- categoria non esiste per i vecchi posts --}}
+    @if ($post->category)
+        <p>{{ $post->category->name }}</p>
+        @else
+        <p>Nessuna categoria selezionata</p>
+    @endif
+    
     <div>
         <a href="{{ route('admin.posts.edit', $post->id) }}">Edit Post</a>
     </div>
