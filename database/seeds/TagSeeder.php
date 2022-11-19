7a00dae7384e5ad125c8070624128a5c77508dda
AlessandroVola <volaalessandro@gmail.com>
2022-11-19 18:12:14 +0100

    seed of tags

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Tag;
+
 
 class TagSeeder extends Seeder
 {
@@ -12,5 +14,14 @@ class TagSeeder extends Seeder
     public function run()
     {
         //
+        $tags = ['Acid','Blues','Jazz','Ambient','Rock','Metal','Punk','Roma','Milano'];
+
+        foreach($tags as $model){
+            $tag = new Tag();
+            $tag->name = $model;
+            $tag->slug = Str::slug($tag->name);
+            $tag->save();
+
+        }
     }
 }
